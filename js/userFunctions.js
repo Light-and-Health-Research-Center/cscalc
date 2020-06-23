dbbed5c82739a8fa451fee1b8a92b2493477ab41
Michael Morrison <morrim5@rpi.edu>
2020-06-23 15:12:40 -0400

    Added functionality to download results

--- js/userFunctions.js
+++ js/userFunctions.js
@@ -250,6 +250,107 @@ function handleHelpMenu(){
 	});
 }
 
+function createResultsJSON(){
+	var str = '{"results" : \n\t{\n';
+
+	// Date
+	var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+	var d = new Date();
+	var date = monthNames[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes();
+  str += '\t\t"date" : "'+date+'",\n';
+
+	//Sources
+	str += '\t\t"sources" : {\n';
+	$("#selected-sources").each(function(){
+		$(this).find('tr').each(function(){
+			var id = $(this).find('td:nth-of-type(1) p').html();
+			str += '\t\t\t"' + id + '" : {\n';
+			str += '\t\t\t\t"info" : {\n'
+			for (var i = 0; i<sourcelist.length; i++){
+				if (sourcelist[i].id == id){
+					str += '\t\t\t\t\t"id" : "' + sourcelist[i].id + '",\n';
+					str += '\t\t\t\t\t"manufacturer" : "' + sourcelist[i].manufacturer + '",\n';
+					str += '\t\t\t\t\t"cct" : "' + sourcelist[i].cct + '",\n';
+					str += '\t\t\t\t\t"lamp" : "' + sourcelist[i].lamp + '",\n';
+					str += '\t\t\t\t\t"desc" : "' + sourcelist[i].info + '",\n';
+					str += '\t\t\t\t\t"spd" : {\n';
+					str += '\t\t\t\t\t\t"wavelengths" : ' + JSON.stringify(sourcelist[i].spd.wavelength) + ',\n';
+					str += '\t\t\t\t\t\t"values" : ' + JSON.stringify(sourcelist[i].spd.value) + '\n';
+					str += '\t\t\t\t\t}\n';
+				}
+			}
+			str += '\t\t\t\t},\n';
+			str += '\t\t\t\t"lux" : "' + $(this).find('td:nth-of-type(2) input').val() + '"\n';
+			if ($(this)[0] == ($("#selected-sources").find('tr:last-child'))[0]){
+				str += '\t\t\t}\n';
+			}else{
+				str += '\t\t\t},\n';
+			}
+		});
+	});
+	str += '\t\t},\n';
+
+	// Biological Input Variables
+	str += '\t\t"bio_variables" : {\n';
+	str += '\t\t\t"mpod" : "' + $('#mpod_sel').val() + '"\n';
+	str += '\t\t},\n';
+
+	// Combined Source Value Metrics
+	str += '\t\t"combined_metrics" : {\n';
+	var vals = [];
+	$("#csvmbody").each(function(){
+		$(this).find('tr').each(function(){
+			vals.push('"'+$(this).find('td:nth-of-type(2)').html() + '"');
+		});
+	});
+	str += '\t\t\t"cs" : ' + vals[0] + ',\n';
+	str += '\t\t\t"cla" : ' + vals[1] + ',\n';
+	str += '\t\t\t"illuminance" : ' + vals[2] + ',\n';
+	str += '\t\t\t"irradiance" : ' + vals[3] + ',\n';
+	str += '\t\t\t"flux" : ' + vals[4] + ',\n';
+	str += '\t\t\t"eml" : ' + vals[5] + ',\n';
+	str += '\t\t\t"cct" : ' + vals[6] + ',\n';
+	str += '\t\t\t"duv" : ' + vals[7] + ',\n';
+	str += '\t\t\t"cri" : ' + vals[8] + ',\n';
+	str += '\t\t\t"gai" : ' + vals[9] + ',\n';
+	str += '\t\t\t"color_coordinates" : ' + vals[10] + '\n';
+	str += '\t\t},\n';
+
+	// Combined spd's
+	str += '\t\t"combined_spds" : {\n';
+	str += '\t\t\t"relative" : {\n';
+	var wl = [], val = [];
+	$("#RelSpdTable").each(function(){
+		$(this).find('tr').each(function(){
+			wl.push(parseFloat($(this).find('td:nth-of-type(1)').html()));
+			val.push(parseFloat($(this).find('td:nth-of-type(2)').html()));
+		})
+	});
+	str += '\t\t\t\t"wavelengths" : [' + wl + '],\n';
+	str += '\t\t\t\t"values" : [' + val + ']\n';
+	str += '\t\t\t},\n';
+
+	str += '\t\t\t"absolute" : {\n';
+	var wl = [], val = [];
+	$("#AbsSpdTable").each(function(){
+		$(this).find('tr').each(function(){
+			wl.push(parseFloat($(this).find('td:nth-of-type(1)').html()));
+			val.push(parseFloat($(this).find('td:nth-of-type(2)').html()));
+		})
+	});
+	str += '\t\t\t\t"wavelengths" : [' + wl + '],\n';
+	str += '\t\t\t\t"values" : [' + val + ']\n';
+	str += '\t\t\t}\n';
+	str += '\t\t}\n';
+
+	str += '\t}\n}';
+
+  $('#jsondownload').attr('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(str));
+  $('#jsondownload').attr('download', 'CSCalculator Results.json');
+  $('#jsondownload')[0].click();
+}
+
 
 // Page Action Functions
 $(document).ready(function(){
@@ -325,4 +426,6 @@ $(document).ready(function(){
 	handleSPDBtns();
 
 	handleHelpMenu();
+
+	$("#resultsDownload").click(createResultsJSON);
 });
