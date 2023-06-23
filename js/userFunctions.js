bd833cb148c86450eb23cd3d67228afaf5ad1676
terryxiaye <136369996+terryxiaye@users.noreply.github.com>
2023-06-23 14:55:52 -0400

    updated notes text & out of range messages. Made notes collapsible

--- js/userFunctions.js
+++ js/userFunctions.js
@@ -1139,10 +1139,12 @@ function updateResults() {
 
   $("#timeDiv").show();
   $("#timeNotes").hide();
-  if(time<0.5||time>3.0)
-    $("#resultTime").html("Exposure duration is out of range. Try changing the light level.");
+  if(time<0.5)
+    $("#resultTime").html("Exposure duration is out of range. Decrease the light level.");
+  else if(time>3.0)
+    $("#resultTime").html("Exposure duration is out of range. Increase the light level.");
   else
-    $("#resultTime").html("You would need an exposure duration of "+time+" h at CS  of "+combinedValues.CS.toFixed(3)+" to achieve a CS<sub>d</sub> of 0.43");
+    $("#resultTime").html("As per UL DG 24480, you would need an exposure duration of "+time+" h at CS  of "+combinedValues.CS.toFixed(3)+" to achieve a CS<sub>d</sub> of 0.43");
 
   // Update Relative SPD HTML
   $("#RelSpdContainer").empty();
